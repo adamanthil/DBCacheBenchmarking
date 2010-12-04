aa9e4086fcd0e349a38fa815df10b10090ff77c5
Emma Turetsky <turetsky@cs.wisc.edu>
2010-12-03 21:38:10 -0500

    Added a hard coded version of the data generator for testing purposes.

---
+++ DataCreator.cpp
@@ -0,0 +1,9 @@
+#include "DataCreator.h"
+
+DataCreator::DataCreator(const std::string & configFile)
+{
+  std::ofstream oFile ("Student.tab", std::ios::out | std::ios::binary);
+  char * data = "02389389392Greig               Hazell              M0101SR0238938938213949493484Andrew              Bender              M2001JR23879093894Emma                Turetsky            F2010SR";
+  oFile.write(data, 183);
+  oFile.close();
+}
