b0a3fa17a023a3e7cb41d9ec31a01935e6477259
Emma Turetsky <turetsky@cs.wisc.edu>
2010-12-05 18:29:08 -0500

    Again

--- DataCreator.cpp
+++ DataCreator.cpp
@@ -2,6 +2,7 @@
 
 void DataCreator::Create(const std::string & configFile)
 {
+ // do somethin
   std::ofstream oFile ("Student.tab", std::ios::out | std::ios::binary);
   const char * data = "02389389392Greig               Hazell              M0101SR0238938938213949493484Andrew              Bender              M2001JR23879093894Emma                Turetsky            F2010SR";
   oFile.write(data, 183);
