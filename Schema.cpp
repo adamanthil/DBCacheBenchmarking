98bdbebbb87c2086331c84942794dc29b4d056e3
Greig Hazell <hazell@wisc.edu>
2010-12-05 09:18:34 -0600

    JoinOperator.cpp
    Final Fix, allow filtering and self-joins

--- Schema.cpp
+++ Schema.cpp
@@ -12,19 +12,23 @@ Schema::~Schema()
   std::for_each(begin(), end(), free);
 }
 
-void Schema::add(const Attribute * attribute)
+void Schema::add(const Attribute * a)
 {
-  Attribute * a = new Attribute(attribute->id(),
-				attribute->name(),
-				attribute->table(),
-				attribute->size(),
-				attribute->type());
-  push_back(a);
+  Attribute * attribute = new Attribute(size(), 
+				a->name(),
+				a->table(),
+				a->size(),
+				a->type());
+  push_back(attribute);
+  m_offset.push_back(m_size);
   m_size += attribute->size();
 }
 
 int Schema::offset(const Attribute * attribute) const
 {
+
+  return m_offset[attribute->id()];
+  /*
   int offset = 0;
   for (int i = 0; i < size(); i++)
     {
@@ -37,6 +41,7 @@ int Schema::offset(const Attribute * attribute) const
     }
   
   return -1;
+  */
 }
 
 size_t Schema::rsize() const
@@ -62,4 +67,14 @@ const Attribute * Schema::operator[](const std::string & name) const
     }
 
   return NULL;
+}
+
+const Attribute * Schema::operator[](int fid) const
+{
+  return std::vector<Attribute *>::at(fid);
+}
+
+const Attribute * Schema::at(int fid) const
+{
+  return std::vector<Attribute *>::at(fid);
 }
