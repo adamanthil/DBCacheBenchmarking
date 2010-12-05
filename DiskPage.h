8320bf6f43fe3abfd4dce0e343e9e832757b893c
Emma Turetsky <turetsky@cs.wisc.edu>
2010-12-05 15:18:08 -0500

    DiskPage.h

--- DiskPage.h
+++ DiskPage.h
@@ -21,6 +21,7 @@ class DiskPage
   size_t size() const;
   size_t capacity() const;
   void get(int rid, const Schema * fields, byte * buffer, size_t length);
+  void get(int rid, const std::vector<Attribute *> fields, byte * buffer, size_t length);
   int put(int rid, const char * field, char * buffer, size_t length);
 };
 
