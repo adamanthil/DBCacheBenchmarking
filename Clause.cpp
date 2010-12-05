9e563398ca4f6986d8571211b655d4a8102fb4b9
Adamanthil <igothelp@gmail.com>
2010-12-05 16:16:06 -0600

    Adding SelectionList to JoinClause

--- Clause.cpp
+++ Clause.cpp
@@ -38,14 +38,26 @@ bool WhereClause::evaluate(const Tuple & t)
   return m_expression.evaluate();
 }
 
-JoinClause::JoinClause(BooleanExpression & exp, const Schema * schema[],
+JoinClause::JoinClause(BooleanExpression & exp, SelectionList items[],
 		       std::vector<IVariableOperand *> variables[]) 
   : m_expression(exp) 
 {
-  m_tuple[LEFT].m_data = NULL;
-  m_tuple[RIGHT].m_data = NULL;
-  m_tuple[LEFT].schema(schema[LEFT]);
-  m_tuple[RIGHT].schema(schema[RIGHT]);
+	
+	for(int i = 0; i < 2; i++) {
+		Schema * schema = new Schema();
+		for(int j = 0; j < items[i].size(); j++) {
+			schema->add(items[i].at(j));
+		}
+		m_tuple[i].m_data = NULL;
+		m_tuple[i].schema(schema);
+		m_items[i] = items[i];
+	}
+}
+
+JoinClause::~JoinClause() {
+	for(int i = 0; i < 2; i++) {
+		delete m_tuple[i].schema();
+	}
 }
 
 const Schema * JoinClause::schema(int branch) const
@@ -58,4 +70,9 @@ bool JoinClause::evaluate(const Tuple & t0, const Tuple & t1)
   m_tuple[LEFT].m_data = t0.m_data;
   m_tuple[RIGHT].m_data = t1.m_data;
   return m_expression.evaluate();
+}
+
+const SelectionList & JoinClause::filter(int branch) const 
+{
+	return m_items[branch];
 }
