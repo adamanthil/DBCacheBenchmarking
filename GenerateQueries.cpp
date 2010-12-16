9e08bd011559cc78406cc21b4fc6946887d8b16c
Adamanthil <igothelp@gmail.com>
2010-12-16 17:26:36 -0600

    initial query generator

---
+++ GenerateQueries.cpp
@@ -0,0 +1,54 @@
+#ifndef APPLICATION_H_
+#define APPLICATION_H_
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <string.h>
+#include <iostream>
+
+// Forward declarations
+void genQuery(int, int);
+void query0(int);
+
+// entry point
+int main(int argc, char ** argv)
+{
+	
+	// initialize random seed
+	srand ( time(NULL) );
+	
+	// Loop through number of arguments (number of queries to run)
+	for(int i = 0; i < argc; i++) {
+		int count = atoi(argv[i]);
+		genQuery(i-1, count);
+	}
+	
+	return 0;
+}
+
+void genQuery(int query, int count) {
+	switch(query) {
+		case 0:
+		query0(count);
+	}
+
+}
+
+void query0(int count) {
+	// Generate query0 "count" times
+	for(int i = 0; i < count; i++) {
+		int number = rand() % 100 + 1;
+		std::cout << "sscan(T1:R; R.a, R.b; R.a < " << number << ")" << std::endl;
+	}
+}
+
+void query1(int count) {
+	// Generate query0 "count" times
+	for(int i = 0; i < count; i++) {
+		int number = rand() % 100 + 1;
+		std::cout << "sscan(T1:R; R.a, R.b; R.a < " << number << ")" << std::endl;
+	}
+}
+
+#endif
