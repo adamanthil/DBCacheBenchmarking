092266a3b4933885df8b5f8e45e3bffc5c3d6cb6
Adamanthil <igothelp@gmail.com>
2010-12-21 14:34:59 -0600

    Partial Multithreading...

--- Benchmark.cpp
+++ Benchmark.cpp
@@ -29,13 +29,68 @@
 
 using namespace lexer;
 
-void profile(const std::string & query)
+class ThreadContainer {
+public:
+	int threadNum;
+	std::string * query;
+};
+
+// Declare globals to make multi-threading easier
+bool * threadInUse;
+pthread_t ** threads;
+int numThreads;
+
+void * profile(void * ptr)
 {
+  ThreadContainer * container;
+  container = (ThreadContainer *) ptr;
+	
   Parser p;
-  Query q(0, p.parse(query));
-  q.profile();
+
+  IRelationalOperator * op =  p.parse(*container->query);
+  op->dump(std::cout);
+  delete op;
+
+  //Query q(0, p.parse(*container->query));
+  //q.profile();
+
+  // mark thread as completed
+  threadInUse[container->threadNum] = false;
+
+  // Free memory
+  delete container->query;
+  delete container;
+
+  pthread_exit(0);
 }
 
+void initializeThreads() {
+	threads = new pthread_t*[numThreads];	// Array of pointers to threads
+	for(int i = 0; i < numThreads; i++) {
+		threads[i] = new pthread_t();
+	}
+}
+
+void destructThreads() {
+	for(int i = 0; i < numThreads; i++) {
+		delete threads[i];
+	}
+	delete[] threads;
+}
+
+// Looks in bool array to determine the next available thread and saves offset in nextThread
+bool availableThread(int & nextThread) {
+	for(int i = 0; i < numThreads; i++) {
+		if(!threadInUse[i]) {
+			nextThread = i;
+			threadInUse[i] = true;
+			return true;
+		}
+	}
+	return false;
+}
+
+// Initializes the database
 void initialize(const char * catalog = "db.xml", const char * files = "config")
 {
   std::cout << "initializing..."; std::cout.flush();
@@ -60,7 +115,7 @@ int main(int argc, char ** argv)
 	}
 	
 	bool materialization = atoi(argv[1]);	// use materialization
-	int numThreads = atoi(argv[2]); // number of available threads
+	numThreads = atoi(argv[2]); // number of available threads
 	int availThreads = numThreads;	// number of available threads
 	
 	// Setup database
@@ -75,27 +130,44 @@ int main(int argc, char ** argv)
 	Database * db = Database::getInstance();
 	
 	// Setup Threads
-	pthread_t * threads = new pthread_t[numThreads];
-	bool * threadInUse = new bool[numThreads];
+	initializeThreads();	// Initialize array of pointers to threads
+	threadInUse = new bool[numThreads];
 	memset(threadInUse, 0, numThreads);	// Set to 0
 	
 	// Read standard input
 	std::string query;
 	getline(std::cin, query);
-	//std::cin >> query;
+	
 	int queryNum = 0;
+	pthread_t nextThread;
+	int threadOffset = -1;
+	
+	// Loop till there are no more queries to execute
 	while(query != "end" && query != "quit" && query != "exit") {
 		
-		std::cout << "Executing query "<< queryNum << std::endl;
-		
-		profile(query);
-		
-		getline(std::cin, query);
-		queryNum++;
+		// Check if there is an available thread
+		if(availableThread(threadOffset)) {
+
+			std::cout << "Executing query "<< queryNum << " in thread " << threadOffset << std::endl;
+
+			// Instantiate a new container to pass to the thread.  Thread will delete it
+			ThreadContainer * container = new ThreadContainer();
+			container->threadNum = threadOffset;
+			container->query = new std::string();
+			
+			int retVal = pthread_create( threads[threadOffset], NULL, profile, (void*) &container);
+			
+			//profile(query);
+
+			getline(std::cin, query);
+			queryNum++;
+		}
+
 	}
 	
 	// memory cleanup
-	delete[] threads;
+	destructThreads();
+	delete[] threadInUse;
 	
 	return 0;
 }
