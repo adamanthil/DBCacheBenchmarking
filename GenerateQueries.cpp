deaaf7b24bef91a0090c0764e2323865015ce6a5
Adamanthil <igothelp@gmail.com>
2010-12-21 22:43:41 -0600

    Adding non-optimized queries to query generator

--- GenerateQueries.cpp
+++ GenerateQueries.cpp
@@ -9,17 +9,25 @@
 #include <string.h>
 #include <time.h>
 
+// Number of optimized queries (ugly and hardcoded since the queries are hardcoded - sadface )
+const int numOptimized = 15;
+
 const char * getOperator(int index) {
 	switch(index) {
 		case 0:
-		return "<";
+			return "<";
 		case 1:
-		return "=";
+			return ">";
+		case 3: // Skip 2 so that != is default.  EQ will only be returned if index is >= 3
+			return "=";
 		default:
-		return ">";
+			return "!=";
 	}
 }
 
+// ----------------------------------------------------------
+// Queries Optimized for Partition Scheme
+// ----------------------------------------------------------
 // Template: sscan(T1:t; t.a, t.b, t.n, t.o; )
 void query0(std::stringstream & text) {
 	text << "sscan(T1:t; t.a, t.b, t.n, t.o; )" << std::endl;
@@ -38,25 +46,25 @@ void query1(std::stringstream & text) {
 // Template: sscan(T1:t; t.l, t.m, t.q; t.l < t.m)
 void query2(std::stringstream & text) {
 	int random = rand() % 3;
-	const char * sign = getOperator(random);
-	text << "sscan(T1:t; t.l, t.m, t.q; t.l " << sign << " t.m)" << std::endl;
+	const char * op = getOperator(random);
+	text << "sscan(T1:t; t.l, t.m, t.q; t.l " << op << " t.m)" << std::endl;
 }
 
 // Template: sscan(T2:t; t.b, t.l, t.m; t.b < 500 | t.b > 3300 & t.b < 4573 | t.b > 98000)
 void query3(std::stringstream & text) {
-	const char * sign1 = getOperator(rand() % 3);
-	const char * sign2 = getOperator(rand() % 3);
+	const char * op1 = getOperator(rand() % 4);
+	const char * op2 = getOperator(rand() % 4);
 	int number1 = rand() % 500 + 1;
 	int number2 = 80000 + (rand() % 20000 + 1);
-	text << "sscan(T2:t; t.b, t.l, t.m; t.b " << sign1 << " " << number1 << " | t.b  > " 
-		<< "3300 & t.b < 4573 | t.b " << sign2 << " " << number2 << ")" << std::endl;
+	text << "sscan(T2:t; t.b, t.l, t.m; t.b " << op1 << " " << number1 << " | t.b  > " 
+		<< "3300 & t.b < 4573 | t.b " << op2 << " " << number2 << ")" << std::endl;
 }
 
 // Template: sscan(T2:t; t.b, t.m, t.l; t.l > t.m)
 void query4(std::stringstream & text) {
-	int random = rand() % 3;
-	const char * sign = getOperator(random);
-	text << "sscan(T2:t; t.b, t.m, t.l; t.l " << sign << " t.m)" << std::endl;
+	int random = rand() % 4;
+	const char * op = getOperator(random);
+	text << "sscan(T2:t; t.b, t.m, t.l; t.l " << op << " t.m)" << std::endl;
 }
 
 // Template: sscan(T3:t; t.b, t.m, t.q; t.m = ‘A’ | t.m = ‘E’ | t.m = ‘I’ | t.m = ‘O’ | t.m = ‘U’)
@@ -71,9 +79,102 @@ void query5(std::stringstream & text) {
 		<< "' | t.m = '" << char3 << "' | t.m = '" << char4 << "' | t.m = '" << char5 << "')" << std::endl;
 }
 
-// Template: sscan(T3:t; t.a, t.h; t.a < 3800 | t.a > 50000 & t.h = ‘H’)
+// Template: sscan(T3:t; t.a, t.h; t.a < 3800 | t.a > 50000 & t.h = 'H')
 void query6(std::stringstream & text) {
-	text << "sscan(T3:t; t.a, t.h; t.a < 3800 | t.a > 50000 & t.h = ‘H’)" << std::endl;
+	int number1 = rand() % 4000 + 1;
+	int number2 = 40000 + (rand() % 1000 + 1);
+	char char1 = (char)(rand() % 26 + 65);
+	text << "sscan(T3:t; t.a, t.h; t.a < " << number1 << " | t.a > " << number2 << " & t.h = '" << char1 << "')" << std::endl;
+}
+
+// Template: sscan(T4:t; t.a, t.b, t.c, t.l, t.m, t.p, t.n, t.o; )
+void query7(std::stringstream & text) {
+	text << "sscan(T4:t; t.a, t.b, t.c, t.l, t.m, t.p, t.n, t.o; )" << std::endl;
+}
+
+// Template: sscan(T4:t; t.a, t.f, t.g, t.i, t.m, t.n, t.n, t.o, t.q, t.r; t.m < t.n)
+void query8(std::stringstream & text) {
+	const char * op1 = getOperator(rand() % 3);
+	text << "sscan(T4:t; t.a, t.f, t.g, t.i, t.m, t.n, t.n, t.o, t.q; t.m " << op1 << " t.n)" << std::endl;
+}
+
+// Template: sscan(T5:t; t.l, t.m, t.n, t.q; )
+void query9(std::stringstream & text) {
+	text << "sscan(T5:t; t.l, t.m, t.n, t.q; )" << std::endl;
+}
+
+// Template: sscan(T5:t; t.l, t.m, t.n, t.q; t.m > t.n & t.l = 'Z')
+void query10(std::stringstream & text) {
+	char char1 = (char)(rand() % 26 + 65);
+	const char * op1 = getOperator(rand() % 3);
+	text << "sscan(T5:t; t.l, t.m, t.n, t.q; t.m " << op1 << " t.n & t.l = '" << char1 << "')" << std::endl;
+}
+
+// Template: merge-join(sscan(T1:R; R.a, R.b, T.n, R.o; ), sscan(T3:S; S.a, S.h, S.r; S.h = 'A' | S.h = 'B'); R.a = S.a; )
+void query11(std::stringstream & text) {
+	char char1 = (char)(rand() % 26 + 65);
+	char char2 = (char)(rand() % 26 + 65);
+	text << "merge-join(sscan(T1:R; R.a, R.b, T.n, R.o; ), sscan(T3:S; S.a, S.h, S.r; S.h = '" 
+		<< char1 << "' | S.h = '" << char2 <<"'); R.a = S.a; )" << std::endl;
+}
+
+// Template: merge-join(sscan(T1:R; R.a, R.b, T.n, R.o; R.a < 5500), sscan(T3:S; S.a, S.h, S.r; S.h = 'A' | S.h = 'B'); R.a = S.a; )
+void query12(std::stringstream & text) {
+	int number = rand() % 10000 + 1;
+	char char1 = (char)(rand() % 26 + 65);
+	char char2 = (char)(rand() % 26 + 65);
+	text << "merge-join(sscan(T1:R; R.a, R.b, T.n, R.o; R.a < " << number 
+		<< "), sscan(T3:S; S.a, S.h, S.r; S.h = '" << char1 << "' | S.h = '" << char2 << "'); R.a = S.a; )" << std::endl;
+}
+
+// Template: merge-join(sscan(T3:R; R.a, R.r; ), sscan(T5:S; S.f, S.e, S.p; S.e < 5); R.a = S.f; )
+void query13(std::stringstream & text)
+{
+  const char * op = getOperator(rand() % 4);
+  int number = rand() % 10 + 1;
+  text << "merge-join(sscan(T3:R; R.a, R.r; ), sscan(T5:S; S.f, S.e, S.p; S.e " << op << " " << number << "); R.a = S.f; )" << std::endl;
+}
+
+
+//  Template: merge-join(merge-join(sscan(T1:S; S.a, S.b, S.o; ), sscan(T3:R; R.b, R.m, R.q; R.b >= 4000 & R.m < 'F'); R.b = S.a;), sscan(T5:T; T.d, T.f; ); T.f = R.a; )
+void query14(std::stringstream & text)
+{
+	int number = rand() % 10000 + 1;
+	char char1 = (char)(rand() % 26 + 65);
+	text << "merge-join(merge-join(sscan(T1:S; S.a, S.b, S.o; ), "
+		<< "sscan(T3:R; R.b, R.m, R.q; R.b >= " << number << " & R.m < '" << char1 << "'); R.b = S.a;)," 
+		<< " sscan(T5:T; T.d, T.f; ); T.f = R.a; )" << std::endl;
+}
+
+// ----------------------------------------------------------
+// Queries Not Optimized for Partition Scheme
+// ----------------------------------------------------------
+
+// Template: sscan(T1:t; t.a, t.n, t.c, t.e, t.m, t.q; )
+void query15(std::stringstream & text) {
+	text << "sscan(T1:t; t.a, t.n, t.c, t.e, t.m, t.q; )" << std::endl;
+}
+
+// Template: sscan(T3:t; t.a, t.h, t.f, t.c, t.q; T3.m = 'B')
+void query16(std::stringstream & text) {
+	const char * op = getOperator(rand() % 4);
+	char char1 = (char)(rand() % 26 + 65);
+	text << "sscan(T3:t; t.a, t.h, t.f, t.c, t.q; T3.m " << op << " '" << char1 << "')" << std::endl;
+}
+
+void query17(std::stringstream & text) {
+  const char * op = getOperator(rand() % 4);
+  int number = rand() % 10 + 1;
+  text << "merge-join(sscan(T5:R; R.a, R.o, R.f; ), sscan(T3:S; S.a, S.b, S.c, S.f, S.i, S.q, S.e; S.e " 
+    << op << " " << number << "); R.a = S.f; )" << std::endl;
+}
+
+void query18(std::stringstream & text) {
+  const char * op = getOperator(rand() % 4);
+  char char1 = (char)(rand() % 26 + 65);
+  int number = rand() % 10 + 1;
+  text << "merge-join(sscan(T4:R; R.a, R.o, R.f, R.l; R.l " << op << " " << char1 << "), sscan(T5:S; S.a, S.b, S.c, S.f, S.i, S.q, S.e; S.e " 
+    << op << " " << number << "); R.a = S.f; )" << std::endl;
 }
 
 void genQuery(std::stringstream & text, int query) {
@@ -96,22 +197,81 @@ void genQuery(std::stringstream & text, int query) {
 		case 5:
 			query5(text);
 			break;
+		case 6:
+			query6(text);
+			break;
+		case 7:
+			query7(text);
+			break;
+		case 8:
+			query8(text);
+			break;
+		case 9:
+			query9(text);
+			break;
+		case 10:
+			query10(text);
+			break;
+		case 11:
+			query11(text);
+			break;
+		case 12:
+			query12(text);
+			break;
+		case 13:
+			query13(text);
+			break;
+		case 14:
+			query14(text);
+			break;
+			
+		// Non partition-optimized queries
+		case 15:
+			query15(text);
+			break;
+		case 16:
+			query16(text);
+			break;
+		case 17:
+			query17(text);
+			break;
+		case 18:
+			query18(text);
+			break;
 	}
-
 }
 
 // entry point
 int main(int argc, char ** argv)
 {
-	int totalQueries = 0;
+	// check arguments
+	if(argc > numOptimized + 2) {
+		std::cout << "Supply at most " << (numOptimized + 1) << " arguments: [ratio non-optimized:optimized queries], [numQ1], [numQ2], ..." << std::endl;
+		exit(1);
+	}
+	
+	if(!strcmp(argv[1], "--help") || !strcmp(argv[1], "help")) {
+		std::cout << "Supply at most " << (numOptimized + 1) << " arguments: [ratio non-optimized:optimized queries], [numQ1], [numQ2], ..." << std::endl;
+		exit(0);
+	}
+	
+	int totalOptQueries = 0;	// Total number of optimized queries
 	int * counts = new int[argc - 1];	// Counts of each type of query
 	
+	float ratio = atof(argv[1]);	// Ratio of non-optimized queries to optimized
+	int totalNotOptQueries = 0;
+	int totalQueries = 0;
+	
 	// Loop through number of arguments (number of queries to run)
-	for(int i = 1; i < argc; i++) {
+	for(int i = 2; i < argc; i++) {
 		int count = atoi(argv[i]);
-		counts[i - 1] = count;
-		totalQueries += count;
+		counts[i - 2] = count;
+		totalOptQueries += count;
 	}
+	
+	// Calculate number of non-optimzed queries to add
+	totalNotOptQueries = (int)(ratio * totalOptQueries);
+	totalQueries = totalOptQueries + totalNotOptQueries;
 	
 	// Vector to store queries
 	std::vector<std::stringstream*> * queries = new std::vector<std::stringstream*>();
@@ -119,12 +279,21 @@ int main(int argc, char ** argv)
 	// initialize random seed
 	srand ( time(NULL) );
 	
-	for(int i = 0; i < argc - 1; i++) {
+	// Generate partition-optimized queries
+	for(int i = 0; i < argc - 2; i++) {
 		for(int j = 0; j < counts[i]; j++) {
 			std::stringstream * text = new std::stringstream();
 			genQuery(*text, i);
 			queries->push_back(text);	
 		}
+	}
+	
+	// Generate non optimized queries
+	for(int i = 0; i < totalNotOptQueries; i++) {
+		int queryNum = rand() % 4 + 15;	// Non-optimized queries between 15 & 18
+		std::stringstream * text = new std::stringstream();
+		genQuery(*text, queryNum);
+		queries->push_back(text);
 	}
 	
 	// Randomize queries
