3518a36cd0ab8a5527f4ebdbb387d4766d07e9d3
Adamanthil <igothelp@gmail.com>
2010-12-21 18:02:50 -0600

    Initial random query generation

--- GenerateQueries.cpp
+++ GenerateQueries.cpp
@@ -9,14 +9,71 @@
 #include <string.h>
 #include <time.h>
 
-void query0(std::stringstream & text) {
-	int number = rand() % 100 + 1;
-	text << "sscan(T1:R; R.a, R.b; R.a < " << number << ")" << std::endl;
+const char * getOperator(int index) {
+	switch(index) {
+		case 0:
+		return "<";
+		case 1:
+		return "=";
+		default:
+		return ">";
+	}
 }
 
+// Template: sscan(T1:t; t.a, t.b, t.n, t.o; )
+void query0(std::stringstream & text) {
+	text << "sscan(T1:t; t.a, t.b, t.n, t.o; )" << std::endl;
+}
+
+// Template: sscan(T1:t; t.a, t.b, t.n, t.o; t.a < 500 | t.a > 4800 & t.a < 5300 | t.a > 97000)
 void query1(std::stringstream & text) {
-	int number = rand() % 100 + 1;
-	text << "sscan(T1:R; R.a, R.b; R.a > " << number << ")" << std::endl;
+	int number1 = rand() % 500 + 1;
+	int number2 = 2000 + (rand() % 3000 + 1);
+	int number3 = number2 + (rand() % 1000 + 1);
+	int number4 = 70000 + (rand() % 20000 + 1);
+	text << "sscan(T1:t; t.a, t.b, t.n, t.o; t.a < " << number1 
+		<< " | t.a > " << number2 << " & t.a < " << number3 << " | t.a > " << number4 << ")" << std::endl;
+}
+
+// Template: sscan(T1:t; t.l, t.m, t.q; t.l < t.m)
+void query2(std::stringstream & text) {
+	int random = rand() % 3;
+	const char * sign = getOperator(random);
+	text << "sscan(T1:t; t.l, t.m, t.q; t.l " << sign << " t.m)" << std::endl;
+}
+
+// Template: sscan(T2:t; t.b, t.l, t.m; t.b < 500 | t.b > 3300 & t.b < 4573 | t.b > 98000)
+void query3(std::stringstream & text) {
+	const char * sign1 = getOperator(rand() % 3);
+	const char * sign2 = getOperator(rand() % 3);
+	int number1 = rand() % 500 + 1;
+	int number2 = 80000 + (rand() % 20000 + 1);
+	text << "sscan(T2:t; t.b, t.l, t.m; t.b " << sign1 << " " << number1 << " | t.b  > " 
+		<< "3300 & t.b < 4573 | t.b " << sign2 << " " << number2 << ")" << std::endl;
+}
+
+// Template: sscan(T2:t; t.b, t.m, t.l; t.l > t.m)
+void query4(std::stringstream & text) {
+	int random = rand() % 3;
+	const char * sign = getOperator(random);
+	text << "sscan(T2:t; t.b, t.m, t.l; t.l " << sign << " t.m)" << std::endl;
+}
+
+// Template: sscan(T3:t; t.b, t.m, t.q; t.m = ‘A’ | t.m = ‘E’ | t.m = ‘I’ | t.m = ‘O’ | t.m = ‘U’)
+void query5(std::stringstream & text) {
+	char char1 = (char)(rand() % 26 + 65);
+	char char2 = (char)(rand() % 26 + 65);
+	char char3 = (char)(rand() % 26 + 65);
+	char char4 = (char)(rand() % 26 + 65);
+	char char5 = (char)(rand() % 26 + 65);
+	
+	text << "sscan(T3:t; t.b, t.m, t.q; t.m = '" << char1 << "' | t.m = '" << char2 
+		<< "' | t.m = '" << char3 << "' | t.m = '" << char4 << "' | t.m = '" << char5 << "')" << std::endl;
+}
+
+// Template: sscan(T3:t; t.a, t.h; t.a < 3800 | t.a > 50000 & t.h = ‘H’)
+void query6(std::stringstream & text) {
+	text << "sscan(T3:t; t.a, t.h; t.a < 3800 | t.a > 50000 & t.h = ‘H’)" << std::endl;
 }
 
 void genQuery(std::stringstream & text, int query) {
@@ -26,6 +83,18 @@ void genQuery(std::stringstream & text, int query) {
 			break;
 		case 1:
 			query1(text);
+			break;
+		case 2:
+			query2(text);
+			break;
+		case 3:
+			query3(text);
+			break;
+		case 4:
+			query4(text);
+			break;
+		case 5:
+			query5(text);
 			break;
 	}
 
@@ -61,8 +130,12 @@ int main(int argc, char ** argv)
 	// Randomize queries
 	random_shuffle(queries->begin(), queries->end());
 	
+	std::stringstream * text = new std::stringstream();
+	*text << "end" << std::endl;
+	queries->push_back(text);
+	
 	// print queries to standard out
-	for(int i = 0; i < totalQueries; i++) {
+	for(int i = 0; i < totalQueries + 1; i++) {
 		std::cout << queries->at(i)->str();
 		
 		// clean up memory while we're at it
